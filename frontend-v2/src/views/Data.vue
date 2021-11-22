fd961305817cd9f16c29f28a6136ed60192359e8
Kyle <kyle4346@gmail.com>
2021-11-21 19:01:09 -0600

    stuff

--- frontend-v2/src/views/Data.vue
+++ frontend-v2/src/views/Data.vue
@@ -15,6 +15,37 @@
     
   </tr>
 </table>
+
+    <div id="app">
+    <p>
+      <span class="pg" v-for="paragraph in letter" :key="paragraph">
+          {{paragraph}}
+      </span>
+    </p>
+    </div>
   </div>
+
+  
 </template>
+
+<script>
+import Letter from '../assets/H3.txt'
+
+export default {
+  name: 'App',
+  data: function () {
+      return {
+          letter: Letter.split("\n\n")
+      }
+  },
+  components: {
+  }
+}
+</script>
+
+<style>
+.pg {
+    display: block;
+}
+</style>
 
