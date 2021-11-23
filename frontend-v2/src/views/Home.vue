ffc994d9996914634014e0489e72d559fd1730a4
cdobami <cdobami@cougarnet.uh.edu>
2021-11-22 18:37:49 -0600

    test

--- frontend-v2/src/views/Home.vue
+++ frontend-v2/src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
-  <div class="home">
+  <div class="home" align="center">
+    <body background="2.jpg"/>
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to SmartSoft BioInformatics"/>
   </div>
 </template>
 
