96444f854ff185cc4bcb38711617c5135496bdb0
Kyle <kyle4346@gmail.com>
2022-01-30 15:11:41 -0600

    update again

--- frontend-v2/src/views/Data.vue
+++ frontend-v2/src/views/Data.vue
@@ -6,22 +6,26 @@
     <table class="table table-striped table-borders">
       <thead>
         <tr>
-          <th>ID</th>
-          <th>Item</th>
-          <th>Price</th>
-          <th>Quantity</th>
-          <th>Actions</th>
+          <th>RED</th>
+          <th>IR</th>
+          <th>HR</th>
+          <th>HR Valid</th>
+          <th>SPO2</th>
+          <th>SPO2 Valid</th>
         </tr>
       </thead>
       <tbody>
         
-        <tr v-for="user_alias in User" v-bind:key="user_alias._id">
-          <td> {{user_alias._id}}</td>
-          <td>{{user_alias.item}}</td>
-          <td>{{user_alias.price}}</td>
-          <td>{{user_alias.quantity}}</td>
+        <tr v-for="user_alias in User" v-bind:key="user_alias.red">
+          <td> {{user_alias.red}}</td>
+          <td>{{user_alias.ir}}</td>
+          <td>{{user_alias.HR}}</td>
+          <td>{{user_alias.HRvalid}}</td>
+          <td>{{user_alias.SPO2}}</td>
+          <td>{{user_alias.SPO2valid}}</td>
           <td><img alt="trash" src="../assets/trash.jpg">
-          <img alt="Add" src="../assets/Add.png"></td>
+          <img alt="Add" src="../assets/Add.png">
+          <img alt="info" src="../assets/info.png"></td>
           
 
         </tr>
